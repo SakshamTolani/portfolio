2455354b674aa8d2c15f54ce46d52ac007df5695
SakshamTolani <62097813+SakshamTolani@users.noreply.github.com>
2023-01-02 13:53:57 +0530

    Delete backend directory

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDatabase = () =>{
-    mongoose.connect(process.env.MONGO_URI).then(c=>{
-        console.log(`MongoDB Connected Successfully! ${c.connection.host}`);
-    }).catch(err => console.log(err))
-}
