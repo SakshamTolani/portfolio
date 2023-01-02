dc921d3c6842880aeda98f7a11d725babfa7b7f3
SakshamTolani <62097813+SakshamTolani@users.noreply.github.com>
2023-01-02 14:17:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
-import {getFilename, getDirname} from './utils.js';
 export const app = express();
 
 app.use(express.json({limit: "50mb"}));
@@ -11,11 +10,8 @@ app.use(cookieParser());
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
 
-if(process.env.NODE_ENV == "production"){
-    const __filename = getFilename(import.meta.url);
-    const __dirname = getDirname(import.meta.url);
-    app.use(express.static(__dirname,'frontend/build'))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
-}
+app.use(express.static(path.resolve("./frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("./frontend/build/index.html"));
+  });
